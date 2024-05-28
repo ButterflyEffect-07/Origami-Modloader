9bd06e717322874ec387d927901c313c856e1924
Nelucifera <134434617+ButterflyEffect-07@users.noreply.github.com>
2024-05-28 08:27:19 -0400

    added 'Select OS' option

---
+++ Origami/Scripts/backtoinstalled.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public class backtoinstalled : Button
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+private void _on_Button_pressed()
+{
+	GetTree().ChangeScene("res://Installed.tscn");
+}
+
+
+}
+
+
